e4167deb65719adf4fda8abb3252d77469a3d733
Ramesh G <ramesh.guthula.kkd@gmail.com>
2023-08-06 09:29:48 +0530

    adfasdf

--- src/main/java/com/course_project/airlines/controllers/AdminController.java
+++ src/main/java/com/course_project/airlines/controllers/AdminController.java
@@ -1,4 +1,4 @@
-package com.course_project.airlines.controllers;
+package com.course_project.airlines.controllers;sdfd
 
 import com.course_project.airlines.models.Flight;
 import com.course_project.airlines.models.User;
